53a42db6a702dfc5734af1980aa0f4cac33034c1
taweiwS2023 <taweiw@andrew.cmu.edu>
2023-06-06 20:25:25 -0700

    removed googletest submodule

--- Multiplier/src/functions.cpp
+++ Multiplier/src/functions.cpp
@@ -1,12 +1,14 @@
 #include "Multiplier/functions.h"
 #include "Adder/functions.h"
+#include "iostream"
 
 namespace Multiplier {
-    int multiply(int a, int b) {
-        int product = 0;
-        for(int i = 0; i < b; i++) {
-            product = Adder::add(product, a);
-        }
-        return product;
-    }
+int multiply(int a, int b) {
+  int product = 0;
+  for (int i = 0; i < b; i++) {
+    product = Adder::add(product, a);
+  }
+  return product;
+}
+
 }
